1da5777953fdd5f1607cbae4c4ea793644f64b94
caiyingyao <cyy0523xc@gmail.com>
2023-07-10 14:43:41 +0800

    fix bug

--- monitor/main.go
+++ monitor/main.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"net/http"
 	"os"
 	"strings"
@@ -21,42 +22,57 @@ const (
 	methodPost = "post"
 )
 
+// 格式化前的字段
 type Field struct {
-	key    string   // 字段名
-	values []string // key对应的值必须在该列表中，值的元素需要先转化为字符串
+	Key    string        // 字段名
+	Values []interface{} // key对应的值必须在该列表中，值的元素需要先转化为字符串
+}
+
+// 格式化后的字段
+type FmtField struct {
+	Key    string   // 字段名
+	Values []string // 格式化后的字段
 }
 
 type URLConfig struct {
-	url    string  // 监控的url
-	method string  // 方法: get，post等。如果为空，则默认为get
-	any    bool    // 是否只要满足其中一个监控字段即可，默认是所有
-	fields []Field // 需要监控的字段
+	Url       string     // 监控的url
+	Method    string     // 方法: get，post等。如果为空，则默认为get
+	Any       bool       // 是否只要满足其中一个监控字段即可，默认是所有
+	Fields    []Field    // 需要监控的字段
+	fmtFields []FmtField // 格式化后的字段
 }
 
-// 返回值
-type Resp map[string]interface{}
-
-// 转换后的响应值
 type RespDict map[string]string
 
 func main() {
-	println("开始检测url，配置文件: ", configFile)
-	f, err := os.OpenFile(configFile, os.O_RDONLY, 0)
-	if err != nil {
-		panic(fmt.Errorf("打开配置文件%s出错: %s", configFile, err.Error()))
-	}
-	defer f.Close()
-	var data []byte
-	_, err = f.Read(data)
+	// println("开始检测url，配置文件: ", configFile)
+	data, err := ioutil.ReadFile(configFile)
 	if err != nil {
 		panic(fmt.Errorf("配置文件%s读取出错: %s", configFile, err.Error()))
 	}
+	// println(string(data))
 	var urls []URLConfig
 	err = json.Unmarshal(data, &urls)
 	if err != nil {
-		panic(fmt.Errorf("打开配置文件%s出错: %s", configFile, err.Error()))
+		fmt.Println("从配置文件读取的原始数据:", len(data))
+		panic(fmt.Errorf("配置文件%s数据转换出错: %s", configFile, err.Error()))
 	}
-	for _, url := range urls {
+	for idx, url := range urls {
+		if len(url.Url) < 5 {
+			panic(fmt.Errorf("检测url不应该为空: %d: %v", idx, url))
+		}
+		// 格式化字段
+		for _, field := range url.Fields {
+			fmtField := FmtField{
+				Key:    field.Key,
+				Values: []string{},
+			}
+			for _, v := range field.Values {
+				fmtField.Values = append(fmtField.Values, fmt.Sprint(v))
+			}
+			url.fmtFields = append(url.fmtFields, fmtField)
+		}
+		// fmt.Printf("配置数据：%+v\n", url)
 		checkURL(&url)
 	}
 }
@@ -69,16 +85,15 @@ func checkURL(url *URLConfig) {
 	err, resp, text := request(url)
 	if err != nil {
 		errPrint(url, err.Error())
-		return
 	}
-	if len(url.fields) == 0 {
+	if len(url.fmtFields) == 0 {
 		// 如果fields为空，则只要状态码为200则算通过
 		return
 	}
 
 	// 判断字段
 	isAll, isAny := true, false
-	for _, field := range url.fields {
+	for _, field := range url.fmtFields {
 		ok := checkField(&field, resp)
 		if !ok {
 			isAll = false
@@ -86,56 +101,63 @@ func checkURL(url *URLConfig) {
 			isAny = true
 		}
 	}
-	if url.any && isAny {
-		// 只要有一个命中，则命中
-		errPrint(url, text)
-	} else if !url.any && isAll {
-		// 只要有一个不命中，则命中
-		errPrint(url, text)
+	// print(isAll, isAny, url.Any)
+	if url.Any && isAny {
+		// 只要满足一个条件
+		return
+	} else if !url.Any && isAll {
+		// 所有条件都满足
+		return
 	}
-	return
+	errPrint(url, text)
 }
 
 // 请求url并格式化返回值
 func request(url *URLConfig) (err error, respDict RespDict, text string) {
-	url.method = strings.ToLower(url.method)
-	if url.method == "" {
-		url.method = methodGet
+	url.Method = strings.ToLower(url.Method)
+	if url.Method == "" {
+		url.Method = methodGet
 	}
 	var resp *http.Response
-	if url.method == methodGet {
-		resp, err = http.Get(url.url)
+	if url.Method == methodGet {
+		resp, err = http.Get(url.Url)
 		if err != nil {
 			return
 		}
-	} else if url.method == methodPost {
+		defer resp.Body.Close()
+	} else if url.Method == methodPost {
 		var body io.Reader
-		resp, err = http.Post(url.url, "", body)
+		resp, err = http.Post(url.Url, "", body)
 		if err != nil {
 			return
 		}
+		defer resp.Body.Close()
 	}
-	err, text, respDict = fmtResp(resp)
+	// 监控http状态码
+	if !inList(resp.StatusCode) {
+		err = fmt.Errorf("响应状态码%d错误：%x", resp.StatusCode, okStatusList)
+		return
+	}
+	if url.fmtFields != nil {
+		err, text, respDict = checkResp(resp)
+	}
 	return
 }
 
-// 获取响应值
-func fmtResp(resp *http.Response) (err error, text string, respDict RespDict) {
-	if !inList(resp.StatusCode) {
-		err = fmt.Errorf("响应状态码错误：%x", okStatusList)
-		return
-	}
-	var body []byte
-	_, err = resp.Body.Read(body)
+// 检验响应值
+func checkResp(resp *http.Response) (err error, text string, respDict RespDict) {
+	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		return
 	}
 	text = string(body)
-	var respTmp Resp
+	// println("接口响应数据", text)
+	respTmp := make(map[string]interface{})
 	err = json.Unmarshal(body, &respTmp)
 	if err != nil {
 		return
 	}
+	respDict = make(RespDict)
 	for k, v := range respTmp {
 		respDict[k] = fmt.Sprint(v)
 	}
@@ -153,14 +175,14 @@ func inList(status int) bool {
 }
 
 // 判断某个字段是否满足条件
-func checkField(field *Field, resp RespDict) (ok bool) {
-	if val, isIn := resp[field.key]; isIn {
-		if len(field.values) == 0 {
+func checkField(field *FmtField, resp RespDict) (ok bool) {
+	if val, isIn := resp[field.Key]; isIn {
+		if len(field.Values) == 0 {
 			// 如果values为空，则只需要存在key则满足
 			ok = true
 			return
 		}
-		for _, v := range field.values {
+		for _, v := range field.Values {
 			if v == val {
 				ok = true
 				return
@@ -172,5 +194,6 @@ func checkField(field *Field, resp RespDict) (ok bool) {
 
 // 触发url检测异常输出
 func errPrint(url *URLConfig, respText string) {
-	println("异常URL：", url.url, "\n ===> ", respText)
+	fmt.Printf("异常URL: %s\n====> %s\n", url.Url, respText)
+	os.Exit(1)
 }
